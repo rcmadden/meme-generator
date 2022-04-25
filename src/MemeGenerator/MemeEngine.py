eaac7ce276df16916de2e48374422da6bd09b9ad
rcmadden <dataengineer007@gmail.com>
2022-04-24 21:08:06 -0700

    add requirements and stubs for class and methods

---
+++ src/MemeGenerator/MemeEngine.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageFont, ImageDraw
+
+
+# MemeGenerator module with the following responsibilities:
+class MemeEngine():
+    pass
+#     Loading of a file from disk
+
+#     Transform image by resizing to a maximum width 
+# of 500px while maintaining the input aspect ratio
+#     Add a caption to an image (string input) 
+# with a body and author to a random location on the image.
+
+# The class depends on the Pillow library to 
+# complete the defined, incomplete method signatures so 
+# that they work with JPEG/PNG files.
+
+# The method signature to make the meme should be: 
+# make_meme(self, img_path, text, author, width=500) -> str 
+# #generated image path
+    def make_meme(self, img_path, text, author, width=500) -> str:
+        pass
+
+# The init method should take a required argument 
+# for where to save the generated images: __init__(self, output_dir...).
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
