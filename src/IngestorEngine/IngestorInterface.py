088e859fa3e51cd4e50d4c6d954fb96ded519f3c
rcmadden <dataengineer007@gmail.com>
2022-04-24 21:06:10 -0700

    add interface created in strategy-objects exercise

---
+++ src/IngestorEngine/IngestorInterface.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class IngestorInterface(ABC):
+    allowed_extensions = []
+ 
+    @classmethod
+    def can_ingest(cls, path):
+        ext = path.split('.')[-1]
+        return ext in cls.allowed_extensions
+
+    @classmethod
+    @abstractmethod
+    def parse(cls, path):
+        pass
+
+# An abstract method for parsing the file content (i.e., splitting each row) 
+# and outputting it to a Quote object.     
+    
