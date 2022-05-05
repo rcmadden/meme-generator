c1d5b8452e526d5d8d4e79e5d14e49e4bc5378d0
rcmadden <dataengineer007@gmail.com>
2022-05-04 19:57:31 -0700

    add random text placement, replace depreciated use of IMAGE reference and format meme body

--- src/MemeGenerator/MemeEngine.py
+++ src/MemeGenerator/MemeEngine.py
@@ -1,8 +1,10 @@
 from PIL import Image, ImageFont, ImageDraw
+from QuoteEngine import QuoteModel 
+import random
 '''MemeGenerator module with the following responsibilities:
-
-Loading of a file from disk
-
+    Loads a file from disk.
+    Transforms image by resizing to a maximum width of 500px while maintaining the input aspect ratio.
+    Adds a caption to an image (string input) with a body and author to a random location on the image.
 '''
 class MemeEngine():
     def __init__(self, out_dir):
@@ -10,46 +12,30 @@ class MemeEngine():
         Arguments:
             out_dir {str} -- for where to save the generated images.
         Returns:
-            str -- the file path the output images.
+            str -- the file path the output image.
         """
         self.out_dir = out_dir
-        print(self.out_dir)
 
     def make_meme(self, img_path, text, author, width=500) -> str:
-        """Create a meme by adding a caption to an image (string input) 
-        with a body and author to a random location on the image.
-
-        Transform image by resizing to a maximum width 
-        of 500px while maintaining the input aspect ratio
-
-        The class depends on the Pillow library to 
-        complete the defined, incomplete method signatures so 
-        that they work with JPEG/PNG files.
-        """
         self.img_path = img_path
         self.text = text
         self.author = author
         self.width = width
 
-        meme_body = self.text + self.author
-        out_file = 'out.' + self.img_path.split('.')[-1]
-        # out_file = self.img_path.split('.')[-1]
-
-
+        meme_body = f'{self.text} -{self.author}'
+        # meme_body = QuoteModel.model_content(self.text, self.author) #TypeError: QuoteModel.model_content() takes 1 positional argument but 2 were given
+        out_file = '.' + self.img_path.split('.')[-1]
         img = Image.open(img_path)
+        randomYAxis = random.randrange(30, 450, 50)
         if width is not None:
             ratio = width/float(img.size[0])
             height = int(ratio*float(img.size[1]))
-            img = img.resize((width, height), Image.NEAREST)
-    # NEAREST is deprecated and will be removed in Pillow 10 (2023-07-01). Use Resampling.NEAREST or Dither.NONE instead.
+            img = img.resize((width, height), Image.Resampling.NEAREST)
+
         if meme_body is not None:
             draw = ImageDraw.Draw(img)
             font = ImageFont.truetype('./_fonts/LilitaOne-Regular.ttf', size=25)
-            draw.text((10, 30), meme_body, font=font, fill='white')
-        
-        # img.save(self.out_dir + out_file)
+            draw.text((10, randomYAxis), meme_body, font=font, fill='white')
         img.save(self.out_dir + out_file)
-        return self.out_dir + out_file
 
-        #  return out_path of generated meme?  
-        # return img_path, text, author
+        return self.out_dir + out_file
