f38a4e08d8e5e16aefa76c3edc00ce71ac4cc324
rcmadden <dataengineer007@gmail.com>
2022-05-06 18:38:03 -0700

    correct img path and complet route to render in html

--- src/app.py
+++ src/app.py
@@ -6,7 +6,8 @@ from MemeGenerator import MemeEngine
 
 
 app = Flask(__name__)
-meme = MemeEngine('./static')
+app.debug=True
+meme = MemeEngine('./static/tmp')
 
 def setup():
     """ Load all resources """
@@ -15,36 +16,23 @@ def setup():
                    './_data/DogQuotes/DogQuotesPDF.pdf',
                    './_data/DogQuotes/DogQuotesCSV.csv']
 
-    # TODO: Use the Ingestor class to parse all files in the
+    # Use the Ingestor class to parse all files in the
     # quote_files variable
 
     quotes = []
     for f in quote_files:
         quotes.extend(Ingestor.parse(f))
 
-    # TODO: Use the pythons standard library os class to find all
+    # Use the pythons standard library os class to find all
     # images within the images images_path directory
     images = "./_data/photos/dog/"
     imgs = []
     for root, dirs, files in os.walk(images):
         imgs = [os.path.join(root, name) for name in files]
-   
+    
     return quotes, imgs
 
 quotes, imgs = setup()
-# print(quotes, imgs)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, from Russia with love!</p>"
-
-# @app.route("/")
-# def meme_rand():
-    # img = random.choice(imgs)
-
-#     quotes = CSVIngestor.parse(quote_files[0])
-#     path = quotes[0]
-#     return render_template('meme.html', path=path)
 
 @app.route('/')
 def meme_rand():
@@ -52,34 +40,43 @@ def meme_rand():
     img = None
     quote = None
 
-    # @TODO:
     # Use the random python standard library class to:
     # 1. select a random image from imgs array
     img = random.choice(imgs)
     # 2. select a random quote from the quotes array
     quote = random.choice(quotes)
-    print(len(quotes))
-    print(quote.body)
-    print(quote.author)
-    # path = meme.make_meme(img, quote.body, quote.author)
-    # print("IMAGE, QUOTE.BODY, QUOTE.AUTHOR")
-    # print(img, quote.body, quote.author)
-    # return render_template('meme.html', path=path)
+    path = meme.make_meme(img, quote.body, quote.author)
+    return render_template('meme.html', path=path)
+
+
+@app.route('/create', methods=['GET'])
+def meme_form():
+    """ User input for meme information """
+    return render_template('meme_form.html')
+
+
+@app.route('/create', methods=['POST'])
+def meme_post():
+    """ Create a user defined meme """
+
+    # @TODO:
+    # 1. Use requests to save the image from the image_url
+    #    form param to a temp local file.
+    # 2. Use the meme object to generate a meme using this temp
+    #    file and the body and author form paramaters.
+    # 3. Remove the temporary saved image.
+
+    path = None
+
+    return render_template('meme.html', path=path)
 
 
 # TODO:
 # [x] iterate over quote_files
 # [x] refactor Importer
 # [x] call the Importer
-# [] refactor TextIngestor to parse files
-# [] refactor PDFIngestor to parse files
+# [x] refactor TextIngestor to parse files
+# [x] refactor PDFIngestor to parse files
 
-
-# print(CSVIngestor.parse(quote_files[0]))
-
-# quotes = CSVIngestor.parse(quote_files[0])
-hello_world()
-meme_rand()
-
-# if __name__ == "__main__":
-#     app.run()
+if __name__ == "__main__":
+    app.run()
