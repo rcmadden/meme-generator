a2e82de290bfce3d4bb0fcdb00111a2632d55e1c
rcmadden <dataengineer007@gmail.com>
2022-05-07 13:36:11 -0700

    accept user input for url, quote and author

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
 import os
 import random
+from urllib import response
+import requests
+from flask import Flask, render_template, abort, request
+
 from IngestEngine import Ingestor
 from MemeGenerator import MemeEngine
 
+from markupsafe import escape 
 
 app = Flask(__name__)
 app.debug=True
@@ -16,15 +20,10 @@ def setup():
                    './_data/DogQuotes/DogQuotesPDF.pdf',
                    './_data/DogQuotes/DogQuotesCSV.csv']
 
-    # Use the Ingestor class to parse all files in the
-    # quote_files variable
-
     quotes = []
     for f in quote_files:
         quotes.extend(Ingestor.parse(f))
 
-    # Use the pythons standard library os class to find all
-    # images within the images images_path directory
     images = "./_data/photos/dog/"
     imgs = []
     for root, dirs, files in os.walk(images):
@@ -40,10 +39,7 @@ def meme_rand():
     img = None
     quote = None
 
-    # Use the random python standard library class to:
-    # 1. select a random image from imgs array
     img = random.choice(imgs)
-    # 2. select a random quote from the quotes array
     quote = random.choice(quotes)
     path = meme.make_meme(img, quote.body, quote.author)
     return render_template('meme.html', path=path)
@@ -58,25 +54,31 @@ def meme_form():
 @app.route('/create', methods=['POST'])
 def meme_post():
     """ Create a user defined meme """
+    out_path = './static/tmp.'
 
-    # @TODO:
-    # 1. Use requests to save the image from the image_url
-    #    form param to a temp local file.
-    # 2. Use the meme object to generate a meme using this temp
-    #    file and the body and author form paramaters.
-    # 3. Remove the temporary saved image.
+    try:
+        os.remove(out_path + 'jpg')
+    except:
+        print('no file exists')
 
-    path = None
+    try:        
+        os.remove(out_path + 'png')
+    except:
+        'no file to remove'
+    
+    img_url = request.form['image_url']
+    
+    out_file = out_path + img_url.split('.')[-1]
+    response = requests.get(img_url)
+    with open(out_file, 'wb') as img:
+        img.write(response.content)
+
+    body = request.form['body']
+    author = request.form['author']
+    path = meme.make_meme(out_file, body, author)
 
     return render_template('meme.html', path=path)
 
-
-# TODO:
-# [x] iterate over quote_files
-# [x] refactor Importer
-# [x] call the Importer
-# [x] refactor TextIngestor to parse files
-# [x] refactor PDFIngestor to parse files
 
 if __name__ == "__main__":
     app.run()
