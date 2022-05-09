18ad30cbdc82df5f702661745f119bd2a8208ee6
rcmadden <dataengineer007@gmail.com>
2022-05-08 20:00:53 -0700

    add default quote and docstyle cleanup

--- src/app.py
+++ src/app.py
@@ -3,15 +3,15 @@ import random
 from urllib import response
 import requests
 from flask import Flask, render_template, abort, request, redirect, url_for
-from markupsafe import escape 
-
+from markupsafe import escape
+from PIL import Image
 from IngestEngine import Ingestor
 from MemeGenerator import MemeEngine
 
 
 app = Flask(__name__)
-app.debug=True
 meme = MemeEngine('./static/tmp')
+
 
 def setup():
     """ Load all resources """
@@ -28,10 +28,12 @@ def setup():
     imgs = []
     for root, dirs, files in os.walk(images):
         imgs = [os.path.join(root, name) for name in files]
-    
+
     return quotes, imgs
 
+
 quotes, imgs = setup()
+
 
 @app.route('/')
 def meme_rand():
@@ -61,23 +63,38 @@ def meme_post():
     except:
         print('no file exists')
 
-    try:        
+    try:
         os.remove(out_path + 'png')
     except:
         'no file to remove'
-    
+
     img_url = request.form['image_url']
-    
     out_file = out_path + img_url.split('.')[-1]
     response = requests.get(img_url)
+
+    # try:
+    #     with Image.open(response) as im:
+    #         print('verify')
+    #         im.verify()
+    # except:
+    #     'invalid image'
+    # return
+
     with open(out_file, 'wb') as img:
+        print('write')
         img.write(response.content)
 
     body = request.form['body']
     author = request.form['author']
+
+    if body == '':
+        body = 'To be or not to be'
+    if author == '':
+        author = 'Shakespeare'
+
     path = meme.make_meme(out_file, body, author)
     # TODO: handle invalid url/img
-    
+
     return render_template('meme.html', path=path)
 
 
@@ -86,11 +103,10 @@ def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
 
-@app.route('/<name>')
+@app.route('/<name>/')
 def index(name):
     name = escape(name)
     return render_template('page_not_found.html', name=name)
-
 
 
 if __name__ == "__main__":
